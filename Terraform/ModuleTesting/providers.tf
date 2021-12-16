6d828baf690d50fd460dcefc11799bc5e193300d
m-skw <96261229+m-skw@users.noreply.github.com>
2021-12-16 20:43:42 +0100

    corrected

--- Terraform/ModuleTesting/providers.tf
+++ Terraform/ModuleTesting/providers.tf
@@ -1,10 +1,4 @@
 provider "azurerm" {
 features {}
-
-  
-  #subscription_id = "e8181c49-6b6a-4f52-b365-7daebd9f255e"
-  #client_id       = "9838e806-5364-4ff9-ae63-35e2241cc2f8"
-  #client_secret   = "53b6ae5d-259a-4629-ac94-c236fb7f3302"
-  #tenant_id       = "513294a0-3e20-41b2-a970-6d30bf1546fa"
   
 }
